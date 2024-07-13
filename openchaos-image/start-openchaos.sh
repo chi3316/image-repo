1cf49eaef9192f9c71c345753795dc2b0edac48d
JamieChen <jamiechen.778@gmail.com>
2024-07-13 20:34:47 +0800

    Allow empty password

--- openchaos-image/start-openchaos.sh
+++ openchaos-image/start-openchaos.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 JVM_GC_LOG=" -XX:+PrintGCDetails -XX:+PrintGCApplicationStoppedTime  -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=5 -XX:GCLogFileSize=64m  -Xloggc:/dev/shm/benchmark-client-gc_%p.log"
 
 # 启动 OpenChaos
-java $JVM_GC_LOG -jar /app/ChaosControl.jar "$@"
+java $JVM_GC_LOG -jar /chaos-framework/ChaosControl.jar "$@"
 
