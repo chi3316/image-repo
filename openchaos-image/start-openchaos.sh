35a08e02f6f025a46aaaac8d69faea72dcd8320d
JamieChen <jamiechen.778@gmail.com>
2024-07-15 20:42:45 +0800

    Use centod for base image

--- openchaos-image/start-openchaos.sh
+++ openchaos-image/start-openchaos.sh
@@ -2,6 +2,6 @@
 
 JVM_GC_LOG=" -XX:+PrintGCDetails -XX:+PrintGCApplicationStoppedTime  -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=5 -XX:GCLogFileSize=64m  -Xloggc:/dev/shm/benchmark-client-gc_%p.log"
 
-# 启动 OpenChaos
+# start OpenChaos
 java $JVM_GC_LOG -jar /chaos-framework/ChaosControl.jar "$@"
 
